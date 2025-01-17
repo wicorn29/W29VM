39824d419f41b0a9ddc9015421d2b369269a9559
Wilson Corn <wilson.corn@hotmail.com>
2025-01-16 22:27:30 -0800

    Update ExecuteOnBoot.bat

--- Remote/ExecuteOnBoot.bat
+++ Remote/ExecuteOnBoot.bat
@@ -1,3 +1,18 @@
 @echo off
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/wicorn29/W29VM/refs/heads/main/Remote/Deploy/sms.png' -OutFile '%USERPROFILE%\Desktop\wallpaper.png'; $regKey = 'HKCU:\Control Panel\Desktop'; $wallpaper = '%USERPROFILE%\Desktop\wallpaper.png'; Set-ItemProperty -Path $regKey -Name Wallpaper -Value $wallpaper; Set-ItemProperty -Path $regKey -Name WallpaperStyle -Value 2; Set-ItemProperty -Path $regKey -Name TileWallpaper -Value 0; RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters; Remove-Item '%USERPROFILE%\Desktop\wallpaper.png'"
-exit
+setlocal
+
+rem Download the image
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/wicorn29/W29VM/refs/heads/main/Remote/Deploy/sms.png' -OutFile '%USERPROFILE%\Desktop\wallpaper.png'"
+
+rem Set the wallpaper
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%USERPROFILE%\Desktop\wallpaper.png" /f
+reg add "HKCU\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d "2" /f
+reg add "HKCU\Control Panel\Desktop" /v TileWallpaper /t REG_SZ /d "0" /f
+
+rem Refresh the wallpaper
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+
+rem Delete the downloaded image
+del "%USERPROFILE%\Desktop\wallpaper.png"
+
+endlocal
