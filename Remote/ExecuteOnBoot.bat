63b89f572fd9cd3e50afe2dd53bb828009f45c6d
Wilson Corn <wilson.corn@hotmail.com>
2025-01-16 22:32:37 -0800

    Update ExecuteOnBoot.bat

--- Remote/ExecuteOnBoot.bat
+++ Remote/ExecuteOnBoot.bat
@@ -1,18 +1,17 @@
 @echo off
-setlocal
+set wallpaperUrl=https://raw.githubusercontent.com/wicorn29/W29VM/refs/heads/main/Remote/Deploy/sms.png
+set wallpaperPath=%USERPROFILE%\Desktop\wallpaper.png
 
-rem Download the image
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/wicorn29/W29VM/refs/heads/main/Remote/Deploy/sms.png' -OutFile '%USERPROFILE%\Desktop\wallpaper.png'"
+:: Download the image
+powershell -Command "Invoke-WebRequest -Uri '%wallpaperUrl%' -OutFile '%wallpaperPath%'"
 
-rem Set the wallpaper
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%USERPROFILE%\Desktop\wallpaper.png" /f
+:: Set the wallpaper using registry changes
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%wallpaperPath%" /f
 reg add "HKCU\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d "2" /f
 reg add "HKCU\Control Panel\Desktop" /v TileWallpaper /t REG_SZ /d "0" /f
 
-rem Refresh the wallpaper
+:: Refresh the wallpaper
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 
-rem Delete the downloaded image
-del "%USERPROFILE%\Desktop\wallpaper.png"
-
-endlocal
+:: Optionally delete the downloaded file
+del "%wallpaperPath%"
